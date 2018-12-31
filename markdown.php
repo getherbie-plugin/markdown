d6c725e618035d2b1ba57954a0f88ce75d9339dc
tbreuss <thomasbreuss@gmx.ch>
2018-12-31 09:24:45 +0100

    Herbie 2.0 cleanup

--- markdown.php
+++ markdown.php
@@ -1,13 +1,19 @@
 <?php
 
+declare(strict_types=1);
+
 namespace herbie\plugin\markdown;
 
+use Herbie\StringValue;
 use Zend\EventManager\EventInterface;
 use Zend\EventManager\EventManagerInterface;
 
 class MarkdownPlugin extends \Herbie\Plugin
 {
-
+    /**
+     * @param EventManagerInterface $events
+     * @param int $priority
+     */
     public function attach(EventManagerInterface $events, $priority = 1)
     {
         $config = $this->herbie->getConfig();
@@ -19,12 +25,15 @@ class MarkdownPlugin extends \Herbie\Plugin
 
         // add shortcode
         if ((bool)$config->get('plugins.config.markdown.shortcode', true)) {
-            $events->attach('shortcodeInitialized', [$this, 'addShortcode'], $priority);
+            $events->attach('shortcodeInitialized', [$this, 'onShortcodeInitialized'], $priority);
         }
 
         $events->attach('renderContent', [$this, 'onRenderContent'], $priority);
     }
 
+    /**
+     * @param EventInterface $event
+     */
     public function onTwigInitialized(EventInterface $event)
     {
         /** @var Twig_Environment $twig */
@@ -38,32 +47,51 @@ class MarkdownPlugin extends \Herbie\Plugin
         );
     }
 
+    /**
+     * @param EventInterface $event
+     * @throws \Exception
+     */
     public function onRenderContent(EventInterface $event)
     {
         if (!in_array($event->getParam('format'), ['markdown', 'md'])) {
             return;
         }
-        $content = $event->getTarget();
-        $parsed = $this->parseMarkdown($content);
-        $content->set($parsed);
+        /** @var StringValue $stringValue */
+        $stringValue = $event->getTarget();
+        $parsed = $this->parseMarkdown($stringValue->get());
+        $stringValue->set($parsed);
     }
 
-    public function addShortcode(EventInterface $event)
+    /**
+     * @param EventInterface $event
+     */
+    public function onShortcodeInitialized(EventInterface $event)
     {
         /** @var \herbie\plugin\shortcode\classes\Shortcode $shortcode */
         $shortcode = $event->getTarget();
         $shortcode->add('markdown', [$this, 'markdownShortcode']);
     }
 
-    public function parseMarkdown($value)
+    /**
+     * @param string $string
+     * @return string
+     * @throws \Exception
+     */
+    public function parseMarkdown(string $string): string
     {
         $parser = new \ParsedownExtra();
         $parser->setUrlsLinked(false);
-        $html = $parser->text($value);
+        $html = $parser->text($string);
         return $html;
     }
 
-    public function markdownShortcode($attribs, $content)
+    /**
+     * @param mixed $attribs
+     * @param string $content
+     * @return string
+     * @throws \Exception
+     */
+    public function markdownShortcode($attribs, string $content): string
     {
         return $this->parseMarkdown($content);
     }
